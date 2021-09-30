5bb9ca73978f86ca6fd190977dda9bdf320915bb
Brooke Hellerich <brookeh815@cox.net>
2021-09-30 12:14:42 -0500

    commit tree 9752654d22290373760addf5771a625d20552f73

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return`This Saturday, I want to ${activity}!`;
+}
+// saturdayFun();
+// saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(str = '*') {
+    return function(name = 'special') {
+        return `You are ${str}${name}${str}!`
+    }
+}
+wrapAdjective('%')('a dedicated programmer')
